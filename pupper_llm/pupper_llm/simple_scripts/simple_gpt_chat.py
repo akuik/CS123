24ab53dc44c831506a2868a5a34514677da10ae8
JummerCloth <106955815+JummerCloth@users.noreply.github.com>
2024-11-08 21:33:14 +0000

    Update simple_gpt_chat.py

--- pupper_llm/pupper_llm/simple_scripts/simple_gpt_chat.py
+++ pupper_llm/pupper_llm/simple_scripts/simple_gpt_chat.py
@@ -3,7 +3,7 @@ from rclpy.node import Node
 from std_msgs.msg import String
 from openai import OpenAI
 
-client = OpenAI(api_key='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')
+client = OpenAI(api_key='hidden')
 
 class GPT4ConversationNode(Node):
     def __init__(self):
